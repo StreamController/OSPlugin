9a21591f0d1313ede2804a01f85444e59526b2b9
Core447 <jmulmann@protonmail.com>
2023-12-29 11:29:40 +0100

    Add increment config option to volume mixer

--- VolumeMixer/OpenVolumeMixer.py
+++ VolumeMixer/OpenVolumeMixer.py
@@ -5,6 +5,12 @@ from loguru import logger as log
 import os
 from PIL import Image
 
+# Import gtk modules
+import gi
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+from gi.repository import Gtk, Adw
+
 class OpenVolumeMixer(ActionBase):
     ACTION_NAME = "Open Volume Mixer"
     CONTROLS_KEY_IMAGE = False
@@ -14,6 +20,9 @@ class OpenVolumeMixer(ActionBase):
         self.set_default_image(Image.open(icon_path))
 
     def on_key_down(self):
+        # Reset position
+        self.PLUGIN_BASE.start_index = 0
+
         page_path = os.path.join(self.PLUGIN_BASE.PATH, "VolumeMixer", "VolumeMixer.json")
         if not os.path.exists(page_path):
             log.error("Could not find volume mixer page. Consider reinstalling the plugin.")
@@ -24,3 +33,23 @@ class OpenVolumeMixer(ActionBase):
 
         self.PLUGIN_BASE.original_page_path = self.deck_controller.active_page.json_path
         self.deck_controller.load_page(page)
+
+    def get_config_rows(self) -> list:
+        self.increments_row = Adw.SpinRow.new_with_range(min=0, max=100, step=5)
+        self.increments_row.set_title("Increments (%):")
+
+        # Load default
+        settings = self.get_settings()
+        self.increments_row.set_value(settings.get("increments", 10))
+        self.PLUGIN_BASE.volume_increment = self.increments_row.get_value() / 100
+
+        # Connect signal
+        self.increments_row.connect("changed", self.on_increments_change)
+
+        return [self.increments_row]
+    
+    def on_increments_change(self, row):
+        settings = self.get_settings()
+        settings["increments"] = row.get_value()
+        self.PLUGIN_BASE.volume_increment = row.get_value() / 100
+        self.set_settings(settings)
