32f59a10f97b9837c3147a35e424afaec4f85cc9
Core447 <100139110+Core447@users.noreply.github.com>
2024-06-15 09:44:35 +0200

    Use new constructor for GraphBase

--- GraphBase.py
+++ GraphBase.py
@@ -26,10 +26,8 @@ import globals as gl
 from src.Signals import Signals
 
 class GraphBase(ActionBase):
-    def __init__(self, action_id: str, action_name: str,
-                 deck_controller: "DeckController", page: Page, coords: str, plugin_base: PluginBase):
-        super().__init__(action_id=action_id, action_name=action_name,
-            deck_controller=deck_controller, page=page, coords=coords, plugin_base=plugin_base)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.percentages: list[float] = []
 
